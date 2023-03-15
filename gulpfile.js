f62b25e11bdcf8b97129d4284d132d762bab02b6
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-03-15 17:32:30 +0200

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -15,75 +15,90 @@ const server = browser.create();
 const { src, dest, watch, series, parallel } = gulp;
 
 const Path = {
-	STYLES: ["styles/**/*.css", "!styles/**/*.min.css"],
-	ICONS: "icons/**/*.svg",
+  STYLES: ["styles/**/*.css", "!styles/**/*.min.css"],
+  ICONS: "icons/**/*.svg",
 };
 
 export function processStyles() {
-	return src("./styles/styles.css", { sourcemaps: true })
-		.pipe(plumber())
-		.pipe(
-			postcss([
-				atImport(),
-				presetENV(),
-				csso(),
-				calc({
-					precision: 3,
-				}),
-			]).on("error", console.error)
-		)
-		.pipe(
-			rename({
-				extname: ".min.css",
-			})
-		)
-		.pipe(dest("./styles/", { sourcemaps: "." }))
-		.pipe(server.stream());
+  return src("./styles/styles.css", { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(
+      postcss([
+        atImport(),
+        presetENV(),
+        // csso(),
+        calc({
+          precision: 3,
+        }),
+      ]).on("error", console.error)
+    )
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(dest("./styles/", { sourcemaps: "." }))
+    .pipe(server.stream());
 }
 
 export function createStack() {
-	return src(Path.ICONS)
-	.pipe(svgo({
-		floatPrecision: 2,
-	}))
-	.pipe(svgSprite({
-		mode: {
-			view: true,
-			symbol: true
-		}
-	}))
-	.pipe(rename("stack.svg"))
-	.pipe(dest("images/"));
+  return src(Path.ICONS)
+    .pipe(
+      svgo({
+        floatPrecision: 2,
+      })
+    )
+    .pipe(
+      svgSprite({
+        mode: {
+          view: true,
+          symbol: true,
+        },
+      })
+    )
+    .pipe(rename("stack.svg"))
+    .pipe(dest("images/"));
 }
 
 export function validateMarkup() {
-	return src("./*.html")
-		.pipe(htmlValidator.analyzer())
-		.pipe(htmlValidator.reporter({ throwErrors: true }));
+  return src("./*.html")
+    .pipe(htmlValidator.analyzer())
+    .pipe(htmlValidator.reporter({ throwErrors: true }));
 }
 
 export function startServer(done) {
-	server.init({
-		server: {
-			baseDir: "./",
-		},
-		cors: true,
-		notify: false,
-		ui: false,
-		watch: true,
-	});
-	done();
+  server.init({
+    server: {
+      baseDir: "./",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+    watch: true,
+  });
+  done();
 }
 
 function reloadServer() {
-	return server.reload();
+  return server.reload();
 }
 
 function watchFiles() {
-	watch(Path.STYLES, processStyles);
-	watch(Path.ICONS, series(createStack, reloadServer));
-	watch("./**/*.{html,js,jpg,png,svg,ico,webmanifest}", reloadServer);
+  watch(Path.STYLES, processStyles);
+  watch(Path.ICONS, series(createStack, reloadServer));
+  watch("./**/*.{html,js,jpg,png,svg,ico,webmanifest}", reloadServer);
 }
 
-export const compileProject = parallel(processStyles, createStack);
+function copyProject() {
+  return src([
+    "*.{html,md,ico,svg,stylelintrc,editorconfig}",
+    "styles/styles.css",
+    "{images,fonts}/**/*"
+  ]).pipe(dest("build/"));
+}
+
+export const compileProject = series(
+  parallel(processStyles, createStack),
+  copyProject
+);
 export const runDev = series(compileProject, startServer, watchFiles);
